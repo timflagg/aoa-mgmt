0a5b80ba9666370c3e19248ed327af18dd42796e
Alex <alexbrucely@gmail.com>
2022-08-25 16:08:20 -0700

    Update test.sh

--- environment/wave-3/test.sh
+++ environment/wave-3/test.sh
@@ -5,4 +5,4 @@ cluster_context="mgmt"
 sleep 20
 
 # wait for completion of gloo-mesh install
-./tools/wait-for-rollout.sh deployment gloo-mesh-mgmt-server gloo-mesh 10 ${cluster_context}
+../tools/wait-for-rollout.sh deployment gloo-mesh-mgmt-server gloo-mesh 10 ${cluster_context}
