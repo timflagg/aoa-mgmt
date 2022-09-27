d63d06e0c39a9217a6b1704b8e9bee4e519d7fa2
Alex <alexbrucely@gmail.com>
2022-09-27 12:30:13 -0700

    Update init.sh

--- environment/wave-1-clusterconfig/init.sh
+++ environment/wave-1-clusterconfig/init.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-cluster_context="mgmt"
 
 echo "wave description:"
 echo "namespaces, configmaps, secrets"
-
-# create cert-manager namespace without istio injection label
-kubectl create ns cert-manager --context ${cluster_context}
-
 sleep 5
