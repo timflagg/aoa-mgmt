d24cc9f4d5cc96c4d588791c148d4cf7f5fa26fb
Alex <alexbrucely@gmail.com>
2022-09-27 12:21:33 -0700

    unset istio injection from cert-manager ns

--- environment/wave-1-clusterconfig/init.sh
+++ environment/wave-1-clusterconfig/init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+cluster_context="mgmt"
 
 echo "wave description:"
 echo "namespaces, configmaps, secrets"
+
+# create cert-manager namespace without istio injection label
+kubectl create ns cert-manager --context ${cluster_context}
+
 sleep 5
