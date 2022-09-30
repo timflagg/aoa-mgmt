7bdc369fac3184c570b87c1e8175a26895d394af
Alex <alexbrucely@gmail.com>
2022-09-30 12:50:16 -0700

    update init/test

--- environment/6-bookinfo/init.sh
+++ environment/6-bookinfo/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "wave description:"
-echo "deploy bookinfo app"
+echo "deploying httpbin workspace/workspacesettings"
--- environment/7-httpbin/init.sh
+++ environment/7-httpbin/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "wave description:"
-echo "deploy httpbin app"
+echo "deploying httpbin workspace/workspacesettings"
