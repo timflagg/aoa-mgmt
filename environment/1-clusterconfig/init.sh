fecb7c274aa452c6f7dfca4ee1826ffa26fc4c2d
Alex <alexbrucely@gmail.com>
2022-10-21 16:34:23 -0700

    set correct var

--- environment/1-clusterconfig/init.sh
+++ environment/1-clusterconfig/init.sh
@@ -5,4 +5,4 @@ echo "namespaces, configmaps, secrets"
 sleep 5
 
 # create license
-./tools/create-license.sh "${LICENSE_KEY}" "${cluster_context}"
+./tools/create-license.sh "${license_key}" "${cluster_context}"
