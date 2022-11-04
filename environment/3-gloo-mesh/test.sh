ae40d20ae97a4818dbce2dcbfb356e72854ac6a7
Alex <alexbrucely@gmail.com>
2022-11-03 21:37:33 -0700

    Update test.sh

--- environment/3-gloo-mesh/test.sh
+++ environment/3-gloo-mesh/test.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+kubectl apply --context ${cluster_context} -f- <<EOF
+apiVersion: admin.gloo.solo.io/v2
+kind: KubernetesCluster
+metadata:
+  name: mgmt
+  namespace: gloo-mesh
+spec:
+  clusterDomain: cluster.local
+EOF
+
 ./tools/wait-for-rollout.sh deployment ext-auth-service gloo-mesh-addons 10 ${cluster_context}
 ./tools/wait-for-rollout.sh deployment rate-limiter gloo-mesh-addons 10 ${cluster_context}
 ./tools/wait-for-rollout.sh deployment redis gloo-mesh-addons 10 ${cluster_context}
