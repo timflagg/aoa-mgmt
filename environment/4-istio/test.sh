a4866664645ee91e9c14d931b085ac32a76fb153
Alex <alexbrucely@gmail.com>
2022-11-17 13:51:22 -0800

    bump base to 1.15.1

--- environment/4-istio/test.sh
+++ environment/4-istio/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./tools/wait-for-rollout.sh deployment istio-ingressgateway istio-gateways 10 ${cluster_context}
-#./tools/wait-for-rollout.sh deployment istio-eastwestgateway istio-eastwest 10 ${cluster_context}
+./tools/wait-for-rollout.sh deployment istio-ingressgateway-1-15 istio-gateways 10 ${cluster_context}
+#./tools/wait-for-rollout.sh deployment istio-eastwestgateway-1-15 istio-eastwest 10 ${cluster_context}
